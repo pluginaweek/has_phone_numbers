7c7dd246d2dc06e0db3d3f469ce1e6e3dc57002a
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2006-10-24 21:20:09 +0000

    Added missing phoneable_id and type columns.

--- db/migrate/001_create_phone_numbers.rb
+++ db/migrate/001_create_phone_numbers.rb
@@ -1,14 +1,16 @@
 class CreatePhoneNumbers < ActiveRecord::Migration
   def self.up
     create_table :phone_numbers do |t|
+      t.column :phoneable_id,   :integer,   :null => false, :references => nil
       # Default is the United States
-      t.column :country_code,   :string,  :limit => 3,  :null => false, :default => 1
-      t.column :area_code,      :string,  :limit => 4,  :null => false
-      t.column :local_exchange, :string,  :limit => 8,  :null => false
-      t.column :extension,      :string,  :limit => 10
-      t.column :created_at,     :timestamp,             :null => false
-      t.column :updated_at,     :datetime,              :null => false
+      t.column :country_code,   :string,    :null => false, :limit => 3, :default => 1
+      t.column :area_code,      :string,    :null => false, :limit => 4
+      t.column :local_exchange, :string,    :null => false, :limit => 8
+      t.column :extension,      :string,    :limit => 10
+      t.column :created_at,     :timestamp, :null => false            
+      t.column :updated_at,     :datetime,  :null => false
       t.column :deleted_at,     :datetime
+      t.column :type,           :string,    :null => false
     end
   end
   
