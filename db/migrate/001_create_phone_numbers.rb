af3fe9532d3f7ff6e9f34796045de82c354fe0c6
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2006-10-26 01:44:18 +0000

    Fixed missing unsigned attributes for foreign keys.

--- db/migrate/001_create_phone_numbers.rb
+++ db/migrate/001_create_phone_numbers.rb
@@ -1,7 +1,7 @@
 class CreatePhoneNumbers < ActiveRecord::Migration
   def self.up
     create_table :phone_numbers do |t|
-      t.column :phoneable_id,   :integer,   :null => false, :references => nil
+      t.column :phoneable_id,   :integer,   :null => false, :unsigned => true, :references => nil
       # Default is the United States
       t.column :country_code,   :string,    :null => false, :limit => 3, :default => 1
       t.column :area_code,      :string,    :null => false, :limit => 4
