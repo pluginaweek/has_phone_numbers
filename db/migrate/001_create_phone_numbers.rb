0532dace7d8328f5f72f1be2c7e710ba9a2fb6b6
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2007-07-26 03:17:23 +0000

    Update migration formats so that they're more maintainable.

--- db/migrate/001_create_phone_numbers.rb
+++ db/migrate/001_create_phone_numbers.rb
@@ -1,14 +1,14 @@
 class CreatePhoneNumbers < ActiveRecord::Migration
   def self.up
     create_table :phone_numbers do |t|
-      t.column :phoneable_id,   :integer, :null => false, :references => nil
+      t.column :phoneable_id, :integer, :null => false, :references => nil
       t.column :phoneable_type, :string, :null => false
       # Default is the United States
-      t.column :country_code,   :string, :null => false, :limit => 3, :default => 1
-      t.column :number,         :string, :null => false, :limit => 12
-      t.column :extension,      :string, :limit => 10
-      t.column :created_at,     :timestamp, :null => false            
-      t.column :updated_at,     :datetime, :null => false
+      t.column :country_code, :string, :null => false, :limit => 3, :default => 1
+      t.column :number, :string, :null => false, :limit => 12
+      t.column :extension, :string, :limit => 10
+      t.column :created_at, :timestamp, :null => false            
+      t.column :updated_at, :datetime, :null => false
     end
   end
   
