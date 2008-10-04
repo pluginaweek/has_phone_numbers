ae07001902234b5248fc6adc48280f38dc29f15a
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2008-10-04 17:49:37 -0400

    Change how the base module is included to prevent namespacing conflicts

--- lib/has_phone_numbers.rb
+++ lib/has_phone_numbers.rb
@@ -1,12 +1,6 @@
 module PluginAWeek #:nodoc:
   # Adds a generic implementation for dealing with phone numbers
   module HasPhoneNumbers
-    def self.included(base) #:nodoc:
-      base.class_eval do
-        extend PluginAWeek::HasPhoneNumbers::MacroMethods
-      end
-    end
-    
     module MacroMethods
       # Creates the following association:
       # * +phone_number+ - All phone numbers associated with the current record.
@@ -19,5 +13,5 @@ module PluginAWeek #:nodoc:
 end
 
 ActiveRecord::Base.class_eval do
-  include PluginAWeek::HasPhoneNumbers
+  extend PluginAWeek::HasPhoneNumbers::MacroMethods
 end
