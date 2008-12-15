256aa12bd07390b6927e0524e222743fbd84fb3e
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2008-12-14 21:14:38 -0500

    Remove the PluginAWeek namespace

--- lib/has_phone_numbers.rb
+++ lib/has_phone_numbers.rb
@@ -1,17 +1,15 @@
-module PluginAWeek #:nodoc:
-  # Adds a generic implementation for dealing with phone numbers
-  module HasPhoneNumbers
-    module MacroMethods
-      # Creates the following association:
-      # * +phone_number+ - All phone numbers associated with the current record.
-      def has_phone_numbers
-        has_many  :phone_numbers,
-                    :as => :phoneable
-      end
+# Adds a generic implementation for dealing with phone numbers
+module HasPhoneNumbers
+  module MacroMethods
+    # Creates the following association:
+    # * +phone_number+ - All phone numbers associated with the current record.
+    def has_phone_numbers
+      has_many  :phone_numbers,
+                  :as => :phoneable
     end
   end
 end
 
 ActiveRecord::Base.class_eval do
-  extend PluginAWeek::HasPhoneNumbers::MacroMethods
+  extend HasPhoneNumbers::MacroMethods
 end
