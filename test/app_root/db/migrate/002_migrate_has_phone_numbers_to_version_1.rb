760a5097e52b00af6de208abd3c66a57fb7918d1
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2009-04-18 22:50:49 -0400

    Add dependency on Rails 2.3 Remove dependency on plugins_plus

--- test/app_root/db/migrate/002_migrate_has_phone_numbers_to_version_1.rb
+++ test/app_root/db/migrate/002_migrate_has_phone_numbers_to_version_1.rb
@@ -1,9 +1,13 @@
 class MigrateHasPhoneNumbersToVersion1 < ActiveRecord::Migration
   def self.up
-    Rails::Plugin.find(:has_phone_numbers).migrate(1)
+    ActiveRecord::Migrator.new(:up, "#{Rails.root}/../../db/migrate", 0).migrations.each do |migration|
+      migration.migrate(:up)
+    end
   end
   
   def self.down
-    Rails::Plugin.find(:has_phone_numbers).migrate(0)
+    ActiveRecord::Migrator.new(:up, "#{Rails.root}/../../db/migrate", 0).migrations.each do |migration|
+      migration.migrate(:down)
+    end
   end
 end
